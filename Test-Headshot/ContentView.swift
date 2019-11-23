6786a845d29a22eacc5f2273f130b5bd100bce84
dougdougdot3 <doug@dot-3.net>
2019-11-23 08:43:54 -0600

    Initial Commit

---
+++ Test-Headshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test-Headshot
+//
+//  Created by Doug on 2019-11-23.
+//  Copyright © 2019 Shodu Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
